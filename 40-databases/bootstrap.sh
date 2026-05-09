2e3e7f11287b2351d7a8e8b45cb44535b224c9c2
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-09 22:06:36 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-component=$1
+#component=$1
 dnf install ansible -y
 
-cd /home/ec2-user
-git clone https://github.com/KATURI-GOPICHAND/devops-practice.git
+# cd /home/ec2-user
+# git clone https://github.com/KATURI-GOPICHAND/devops-practice.git
 
 
-cd ansible-roboshop-roles-tf
-ansible-playbook -e component=$component roboshop.yaml
+# cd ansible-roboshop-roles-tf
+# ansible-playbook -e component=$component roboshop.yaml
 
