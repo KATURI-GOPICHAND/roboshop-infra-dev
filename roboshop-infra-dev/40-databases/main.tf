0b7492be4298eb2ba610ae4ec4cdf6fc996b0061
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-09 13:12:20 +0530

    terraform

--- roboshop-infra-dev/40-databases/main.tf
+++ roboshop-infra-dev/40-databases/main.tf
@@ -30,7 +30,7 @@ resource "terraform_data" "bootstarp" {
   provisioner "remote-exec" {
     inline = [ 
         "chmod +x /tmp/bootstarp.sh",
-        "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mongodb"
      ]
   }
 }
