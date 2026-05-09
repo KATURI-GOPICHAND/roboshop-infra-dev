cf1790a51c9904380b5f52b3e3f9482273868953
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-09 13:56:09 +0530

    terraform

--- roboshop-infra-dev/40-databases/bootstrap.sh
+++ roboshop-infra-dev/40-databases/bootstrap.sh
@@ -4,7 +4,8 @@ component=$1
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/KATURI-GOPICHAND/ansible-roboshop-roles-tf.git
+git clone https://github.com/KATURI-GOPICHAND/devops-practice.git
+
 
 cd ansible-roboshop-roles-tf
 ansible-playbook -e component=$component roboshop.yaml
