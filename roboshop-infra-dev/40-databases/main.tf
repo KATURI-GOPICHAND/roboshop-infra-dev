9010f1e50f43023f8fd73624b31981c96db94d16
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-09 14:08:43 +0530

    terraform

--- roboshop-infra-dev/40-databases/main.tf
+++ roboshop-infra-dev/40-databases/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "mongodb" {
     local.common_tags
   )
 }
-resource "terraform_data" "bootstarp" {
+resource "terraform_data" "bootstrap" {
   triggers_replace = [
     aws_instance.mongodb.id
   ]
