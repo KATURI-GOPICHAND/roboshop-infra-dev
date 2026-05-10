9c8ca80d0fada4f6252ce3bbd2eabd46f8a523cf
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-10 16:53:43 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -24,7 +24,7 @@ resource "terraform_data" "bootstrap" {
   }
 
   provisioner "file" {
-    source = "bootstrap.sh"  # Local file path
+    source = "${path.module}/bootstrap.sh" # Local file path
     destination = "/tmp/bootstrap.sh" # destination path on the remote machine
   }
 
