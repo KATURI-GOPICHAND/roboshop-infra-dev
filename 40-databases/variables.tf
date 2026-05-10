ac7cd71002a73c92219038db3adf684c76187357
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-10 18:31:07 +0530

    terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z09562662SLZPT00M9LMW"
+}
+
+variable "domain_name" {
+  default = "88sdaws.fun"
+}
