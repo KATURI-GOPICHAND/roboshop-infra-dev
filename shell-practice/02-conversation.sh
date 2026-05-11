9d261632fd7f3d19eb940dc5356637d094a1f785
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-11 22:57:19 +0530

    actions

--- shell-practice/02-conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Gopi : Hello Chand"
-echo "Chand: Hi Gopi"
-echo "Gopi : are you practicing"
-echo "Chand: yes doing"
--- terraform/multi-env/tfvars/prod/backend.tf
+++
@@ -1,7 +0,0 @@
-    bucket = "remote-stateaws-88s-prod" # replace with our unique bucket name
-    key = "remote-state.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    use_lockfile = true
-  
- 
