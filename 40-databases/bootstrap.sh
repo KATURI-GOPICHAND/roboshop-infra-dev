e382efbb473e3c6990b596834605c0a2c738f017
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-16 14:49:34 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -12,4 +12,4 @@ git clone https://github.com/KATURI-GOPICHAND/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
