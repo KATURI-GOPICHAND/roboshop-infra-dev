4171b7ae78655a382fa98c6ae034e558ff80113b
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-05-16 23:37:20 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "catalogue" {
     local.common_tags
   )
 }
-resource "terraform_data" "bootstrap" {
+resource "terraform_data" "catalogue" {
   triggers_replace = [
     aws_instance.catalogue.id
 
